70e49748e7bedf5280a672476a5c95cab37a4fb0
Anna Miklash <aniamiklash@gmail.com>
2021-11-29 17:18:42 +0100

    added additional cors config

--- social_event_support_app/src/main/java/pjatk/socialeventorganizer/social_event_support/security/SecurityConfig.java
+++ social_event_support_app/src/main/java/pjatk/socialeventorganizer/social_event_support/security/SecurityConfig.java
@@ -1,11 +1,17 @@
 package pjatk.socialeventorganizer.social_event_support.security;
 
 import lombok.AllArgsConstructor;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
 import static pjatk.socialeventorganizer.social_event_support.security.enums.AllowedUrlsEnum.*;
 
@@ -14,6 +20,20 @@ import static pjatk.socialeventorganizer.social_event_support.security.enums.All
 @Configuration
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Bean
+    public FilterRegistrationBean corsFilter() {
+        final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true);
+        config.addAllowedOrigin("http://localhost:3000"); // @Value: http://localhost:8080
+        config.addAllowedHeader("*");
+        config.addAllowedMethod("*");
+        source.registerCorsConfiguration("/**", config);
+        FilterRegistrationBean bean = new FilterRegistrationBean(new CorsFilter(source));
+        bean.setOrder(0);
+        return bean;
+    }
 
     @Override
     public void configure(final WebSecurity webSecurity) {
@@ -32,6 +52,9 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .antMatchers(HttpMethod.OPTIONS, "/**").permitAll();
         http.csrf().disable()
                 .authorizeRequests()
                 .anyRequest()
