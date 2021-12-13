ccc695d0831bfb5b87bdc04d61a3465e7e282197
Anna Miklash <aniamiklash@gmail.com>
2021-12-13 18:00:20 +0100

    added rating to location, catering, service

--- social_event_support_app/src/main/java/pjatk/socialeventorganizer/social_event_support/location/controller/LocationController.java
+++ social_event_support_app/src/main/java/pjatk/socialeventorganizer/social_event_support/location/controller/LocationController.java
@@ -29,6 +29,8 @@ public class LocationController {
 
     private final LocationService locationService;
 
+    private final LocationReviewService locationReviewService;
+
     @RequestMapping(
             method = RequestMethod.GET,
             path = "allowed/all",
@@ -47,6 +49,7 @@ public class LocationController {
 
         final ImmutableList<LocationDto> result = ImmutableList.copyOf(list.stream()
                 .map(LocationMapper::toDto)
+                        .peek(dto -> dto.setRating(locationReviewService.getRating(dto.getId())))
                 .collect(Collectors.toList()));
 
         return ResponseEntity.ok(new TableDto<>(new TableDto.MetaDto(count, pageNo, pageSize, sortBy), result));
@@ -125,6 +128,7 @@ public class LocationController {
         return ResponseEntity.ok(
                 ImmutableList.copyOf(list.stream()
                         .map(LocationMapper::toDto)
+                        .peek(locationDto -> locationDto.setRating(locationReviewService.getRating(locationDto.getId())))
                         .collect(Collectors.toList())));
     }
 
@@ -136,6 +140,9 @@ public class LocationController {
     public ResponseEntity<LocationDto> getWithAvailability(@PathVariable long id, @RequestParam String date) {
         final Location location = locationService.getWithAvailability(id, date);
         final LocationDto locationDto = LocationMapper.toDto(location);
+
+        final LocationDto locationDto = LocationMapper.toDto(location);
+        locationDto.setRating(locationReviewService.getRating(id));
 
         return ResponseEntity.ok(locationDto);
     }
@@ -165,6 +172,7 @@ public class LocationController {
         return ResponseEntity.ok(
                 ImmutableList.copyOf(locations.stream()
                         .map(LocationMapper::toDto)
+                        .peek(dto -> dto.setRating(locationReviewService.getRating(dto.getId())))
                         .collect(Collectors.toList())));
     }
 
